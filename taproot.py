aa3197dfcc78a2f28d5e93ce0d89bdb0be36a641
andrewwell7 <nameless_ds@protonmail.com>
2023-03-27 13:19:29 +0200

    First output bug fixed

--- taproot.py
+++ taproot.py
@@ -3,8 +3,8 @@ from bitcoinrpc.authproxy import AuthServiceProxy
 import matplotlib.pyplot as plt
 
 # Connect to the BITPoliTO node using RPC credentials.
-rpc_user = 'bitcoin'
-rpc_password = 'bitcoin'
+rpc_user = 'bitpolito'
+rpc_password = 'bitpolito'
 rpc_host = '127.0.0.1'
 rpc_port = '8332'
 rpc_connection = AuthServiceProxy("http://%s:%s@%s:%s"%(rpc_user, rpc_password, rpc_host, rpc_port), timeout=120)
@@ -18,7 +18,7 @@ ax.set_xlabel("Block Height")
 ax.set_ylabel("Taproot Transactions")
 ax.set_title("Taproot Transactions Since Activation")
 
-# Start analyzing new blocks
+# Start analyzing blocks between current_height and taproot_activation_height
 current_height = rpc_connection.getblockcount()
 
 for i in range(current_height, taproot_activation_height, -1):
@@ -28,32 +28,33 @@ for i in range(current_height, taproot_activation_height, -1):
     taproot_tx_count = 0
     for txid in block["tx"]:
         tx = rpc_connection.getrawtransaction(txid, True)
-        if "taproot" in tx["vout"][0]["scriptPubKey"]["type"]:
-            taproot_tx_count += 1
+        for output in tx["vout"]:
+            if "taproot" in output["scriptPubKey"]["type"]:
+                taproot_tx_count += 1
+                break
     print(f"Block Height: {i}, Transactions: {tx_count}, Taproot Transactions: {taproot_tx_count}")
     ax.plot(i, taproot_tx_count, "bo")
     plt.draw()
 
 while True:
-    # Check if a new block has been added to the blockchain
+    # Check if a new blocks has been added to the blockchain
     new_height = rpc_connection.getblockcount()
     if new_height > current_height:
-        # Analyze the new block
-        block_hash = rpc_connection.getblockhash(new_height)
-        block = rpc_connection.getblock(block_hash)
-        tx_count = len(block["tx"])
-        taproot_tx_count = 0
-        for txid in block["tx"]:
-            tx = rpc_connection.getrawtransaction(txid, True)
-            if "taproot" in tx["vout"][0]["scriptPubKey"]["type"]:
-                taproot_tx_count += 1
+        # Analyze the new blocks
+        for i in range(current_height + 1, new_height + 1):
+            block_hash = rpc_connection.getblockhash(i)
+            block = rpc_connection.getblock(block_hash)
+            tx_count = len(block["tx"])
+            taproot_tx_count = 0
+            for txid in block["tx"]:
+                tx = rpc_connection.getrawtransaction(txid, True)
+                for output in tx["vout"]:
+                    if "taproot" in output["scriptPubKey"]["type"]:
+                        taproot_tx_count += 1
+                        break
         print(f"Block Height: {new_height}, Transactions: {tx_count}, Taproot Transactions: {taproot_tx_count}")
-
-        # Add the new data point to the plot
-        if new_height >= taproot_activation_height:
-            ax.plot(new_height, taproot_tx_count, "bo")
-            plt.draw()
-            plt.pause(0.001)
-
+        ax.plot(i, taproot_tx_count, "bo")
+        plt.draw()
+        plt.pause(0.001)
         current_height = new_height
     time.sleep(1)
