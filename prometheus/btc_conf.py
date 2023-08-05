e5277156c966a9cc6bf003a4c3dee29ef7a1dd48
Lorenzo Canciani <cancianilorenzo@gmail.com>
2023-08-05 11:34:23 +0200

    chore: move auth info to btc_conf.py

---
+++ prometheus/btc_conf.py
@@ -0,0 +1,9 @@
+import os
+RPC_SCHEME = "http"
+RPC_HOST = "localhost"
+RPC_PORT = "8332"
+RPC_USER = "Put_here_your_btc_server_username"
+RPC_PASSWORD = "put_here_your_btc_server_password"
+CONF_PATH = os.environ.get("BITCOIN_CONF_PATH")
+
+TIMEOUT = 30
